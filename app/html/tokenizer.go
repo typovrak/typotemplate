2ec7f228156e9ed3eff0cfff8534f41025e3d2fa
typovrak <typovrak@gmail.com>
2025-06-27 01:06:04 +0200

    first test for the tokenizer html passed!

--- app/html/tokenizer.go
+++ app/html/tokenizer.go
@@ -28,8 +28,13 @@ const (
 	TokenText
 )
 
-func isCharAlphanumeric(char byte) bool {
+func IsCharAlphanumeric(char byte) bool {
 	return (char >= 'a' && char <= 'z') || (char >= '0' && char <= '9')
+}
+
+func addToken(tokens *[]Token, buffer *bytes.Buffer, tokenType TokenType, tokenValue string) {
+	*tokens = append(*tokens, Token{Type: tokenType, Value: tokenValue})
+	buffer.Reset()
 }
 
 func Tokenizer(html string) []Token {
@@ -40,6 +45,10 @@ func Tokenizer(html string) []Token {
 	isBufferAlphanumeric := false
 
 	for i := 0; i < len(html); i++ {
+		if len(buffer.String()) == 0 {
+			isBufferAlphanumeric = true
+		}
+
 		char := html[i]
 
 		// *buffer == < -> TokenTagOpenChar
@@ -54,39 +63,49 @@ func Tokenizer(html string) []Token {
 		// *else -> TokenText
 
 		if char == ' ' && !isBufferInTag {
+			// no need to manage alphanumeric because not in tag
 			buffer.WriteByte(char)
 			continue
 		}
 
 		if char == '<' {
-			tokens = append(tokens, Token{Type: TokenTagOpenChar, Value: string(char)})
-			buffer.Reset()
+			if len(buffer.String()) > 0 {
+				if isBufferAlphanumeric && !isBufferInTag {
+					addToken(&tokens, &buffer, TokenTagName, buffer.String())
+				}
+			}
+
+			addToken(&tokens, &buffer, TokenTagOpenChar, string(char))
 			isBufferInTag = true
 			continue
 		}
 
 		if char == '>' {
-			tokens = append(tokens, Token{Type: TokenTagCloseChar, Value: string(char)})
-			buffer.Reset()
+			if len(buffer.String()) > 0 {
+				if isBufferAlphanumeric && isBufferInTag {
+					addToken(&tokens, &buffer, TokenTagName, buffer.String())
+				}
+			}
+
+			addToken(&tokens, &buffer, TokenTagCloseChar, string(char))
 			isBufferInTag = false
 			continue
 		}
 
 		if char == '/' && len(buffer.String()) == 0 &&
 			len(tokens)-1 >= 0 && tokens[len(tokens)-1].Type == TokenTagOpenChar && tokens[len(tokens)-1].Value == "<" {
+			// TODO: need to do something with the previous non empty buffer
 
-			tokens = append(tokens, Token{Type: TokenTagOpenChar, Value: string(char)})
-			buffer.Reset()
+			addToken(&tokens, &buffer, TokenTagOpenChar, string(char))
 			continue
 		}
 
 		if char != ' ' && isBufferInTag {
-			buffer.WriteByte(char)
-
-			if isBufferAlphanumeric {
-				isBufferAlphanumeric = isCharAlphanumeric(char)
+			if isBufferInTag && isBufferAlphanumeric {
+				isBufferAlphanumeric = IsCharAlphanumeric(char)
 			}
 
+			buffer.WriteByte(char)
 			continue
 		}
 

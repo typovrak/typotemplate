5adc9dba12b705a800cfbd1961a27650bad18cba
typovrak <typovrak@gmail.com>
2025-06-27 00:04:08 +0200

    add / as type tagopenchar

--- app/tests/tokenizer_test.go
+++ app/tests/tokenizer_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"fmt"
 	"testing"
 	"typotemplate/html"
 )
@@ -10,12 +11,53 @@ func TestHTMLTokenizer(t *testing.T) {
 		raw := "<a></a>"
 		tokenized := []html.Token{
 			{
-				Type:  html.TokenTagOpen,
-				Value: "v",
+				Type:  html.TokenTagOpenChar,
+				Value: "<",
+			},
+			{
+				Type:  html.TokenTagName,
+				Value: "a",
+			},
+			{
+				Type:  html.TokenTagCloseChar,
+				Value: ">",
+			},
+			{
+				Type:  html.TokenTagOpenChar,
+				Value: "<",
+			},
+			{
+				Type:  html.TokenTagOpenChar,
+				Value: "/",
+			},
+			{
+				Type:  html.TokenTagName,
+				Value: "a",
+			},
+			{
+				Type:  html.TokenTagCloseChar,
+				Value: ">",
 			},
 		}
 
-		// t.Errorf("error")
+		tokens := html.Tokenizer(raw)
+
+		fmt.Println(tokenized)
+		fmt.Println(tokens)
+
+		if len(tokens) != len(tokenized) {
+			t.Errorf("expected length %d, got %d", len(tokenized), len(tokens))
+		}
+
+		for i := 0; i < len(tokens); i++ {
+			if i < len(tokenized) && tokens[i].Type != tokenized[i].Type {
+				t.Errorf("expected Tokens[%d].Type %d, got %d", i, tokenized[i].Type, tokens[i].Type)
+			}
+
+			if i < len(tokenized) && tokens[i].Value != tokenized[i].Value {
+				t.Errorf("expected Tokens[%d].Value %s, got %s", i, tokenized[i].Value, tokens[i].Value)
+			}
+		}
 	})
 }
 
